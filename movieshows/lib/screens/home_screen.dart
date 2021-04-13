5c6c0e01b6a0c5f66694eb991935f4eeadc225a9
Telat Kaya <telatkaya@yandex.com>
2021-04-14 01:48:24 +0300

    color palette from image

--- movieshows/lib/screens/home_screen.dart
+++ movieshows/lib/screens/home_screen.dart
@@ -12,7 +12,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   AllTrending shows = AllTrending();
   List<AllTrending> showList = [];
-
+  List<String> images = [];
   getData() async {
     var url = Uri.parse(
         "https://api.themoviedb.org/3/trending/all/week?api_key=79f9638dc1bcf9a4e5a09db68640db20&language=tr");
@@ -23,6 +23,8 @@ class _HomeScreenState extends State<HomeScreen> {
       setState(() {
         for (var item in decodedShows["results"]) {
           showList.add(AllTrending.fromJson(item));
+          images.add(
+              "https://image.tmdb.org/t/p/original${AllTrending.fromJson(item).backdropPath}");
         }
       });
       //print(showList[0].name);
@@ -43,7 +45,10 @@ class _HomeScreenState extends State<HomeScreen> {
           width: double.infinity,
           //padding: EdgeInsets.only(top: 10, left: 10, right: 10, bottom: 120),
           height: MediaQuery.of(context).size.height,
-          child: MovieShowsCard(showList: showList),
+          child: MovieShowsCard(
+            showList: showList,
+            images: images,
+          ),
         ),
       ),
     );
--- movieshows/lib/widgets/movie_shows_card1.dart
+++ movieshows/lib/widgets/movie_shows_card1.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:movieshows/models/trending_movies.dart';
 import 'package:palette_generator/palette_generator.dart';
@@ -6,122 +8,130 @@ class MovieShowsCard extends StatefulWidget {
   const MovieShowsCard({
     Key key,
     @required this.showList,
+    @required this.images,
   }) : super(key: key);
 
   final List<AllTrending> showList;
+  final List<String> images;
   @override
   _MovieShowsCardState createState() => _MovieShowsCardState();
 }
 
 class _MovieShowsCardState extends State<MovieShowsCard> {
-  List<PaletteColor> bgColors = [];
+  List<PaletteColor> bgColors;
   int _currentIndex;
-  var colorS = [];
-
-  Future<PaletteGenerator> _generatePalette(var imagePath) async {
-    print('generate çalışıyor..');
-    PaletteGenerator _paletteGenerator =
-        await PaletteGenerator.fromImageProvider(NetworkImage(imagePath),
-            size: Size(110, 150), maximumColorCount: 20);
-    return _paletteGenerator;
-  }
-
-  _updatePalette() async {
-    for (var item in widget.showList) {
-      var path = "https://image.tmdb.org/t/p/original/${item.posterPath}";
-      print("start function");
-
-      _generatePalette(path).then((_palette) {
-        print(_palette.darkMutedColor);
-        _palette.darkMutedColor != null
-            ? bgColors.add(_palette.darkMutedColor)
-            : bgColors.add(PaletteColor(Colors.red, 3));
-      });
-    }
-  }
+  var list = [];
+  var isLoading = true;
 
   @override
   void initState() {
-    _updatePalette();
-    print('init state..');
     super.initState();
     _currentIndex = 0;
+    fetchUserOrder();
+  }
+
+  Future<void> fetchUserOrder() async {
+    Future.delayed(Duration(milliseconds: 700), () async {
+      bgColors = [];
+
+      for (String image in widget.images) {
+        print(image);
+        PaletteGenerator palette = await PaletteGenerator.fromImageProvider(
+            NetworkImage(image),
+            size: Size(200, 100));
+        palette.darkMutedColor != null
+            ? bgColors.add(palette.darkMutedColor)
+            : bgColors.add(PaletteColor(Colors.red, 3));
+      }
+      setState(() {
+        isLoading = false;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Container(
-          width: double.infinity,
-          height: 600,
-          color: bgColors.length > 0
-              ? bgColors[_currentIndex].color
-              : Colors.white,
-          child: PageView(
-            onPageChanged: (index) {
-              setState(() {
-                _currentIndex = index;
-              });
-            },
-            children: widget.showList
-                .map((image) => Container(
-                      padding: const EdgeInsets.all(16.0),
-                      margin: const EdgeInsets.all(16.0),
-                      decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.5),
-                            spreadRadius: 1,
-                            blurRadius: 7,
-                            offset: Offset(0, 3), // changes position of shadow
-                          ),
-                        ],
-                        borderRadius: BorderRadius.circular(30.0),
-                        image: DecorationImage(
-                            image: NetworkImage(
-                                "https://image.tmdb.org/t/p/original${image.posterPath}"),
-                            fit: BoxFit.cover),
-                      ),
-                    ))
-                .toList(),
-          ),
-        ),
-        Expanded(
-          child: Container(
-            padding: const EdgeInsets.all(32.0),
+    //print(widget.images);
+    return isLoading == false
+        ? Container(
+            color: Colors.red,
             width: double.infinity,
-            decoration: BoxDecoration(
-                color: bgColors.length > 0
-                    ? bgColors[_currentIndex].color
-                    : Colors.white),
+            height: MediaQuery.of(context).size.height,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text(
-                  "Nepal, The 8th Wonder",
-                  style: TextStyle(
-                      color: bgColors.isNotEmpty
-                          ? bgColors[_currentIndex].titleTextColor
-                          : Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30.0),
+                Container(
+                  width: double.infinity,
+                  height: 500,
+                  color: bgColors.length > 0
+                      ? bgColors[_currentIndex].color
+                      : Colors.white,
+                  child: PageView(
+                    onPageChanged: (index) {
+                      setState(() {
+                        _currentIndex = index;
+                      });
+                    },
+                    children: widget.showList
+                        .map((image) => Container(
+                              padding: const EdgeInsets.all(16.0),
+                              margin: const EdgeInsets.all(16.0),
+                              decoration: BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.5),
+                                    spreadRadius: 1,
+                                    blurRadius: 7,
+                                    offset: Offset(
+                                        0, 3), // changes position of shadow
+                                  ),
+                                ],
+                                borderRadius: BorderRadius.circular(30.0),
+                                image: DecorationImage(
+                                    image: NetworkImage(
+                                        "https://image.tmdb.org/t/p/original${image.posterPath}"),
+                                    fit: BoxFit.cover),
+                              ),
+                            ))
+                        .toList(),
+                  ),
                 ),
-                const SizedBox(height: 10.0),
-                Text(
-                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id obcaecati tenetur enim et dolore aut dolorum! Fugiat omnis amet atque quos sapiente similique, tempore, vitae eos perferendis cupiditate libero odit.",
-                    textAlign: TextAlign.justify,
-                    style: TextStyle(
-                        color: bgColors.isNotEmpty
-                            ? bgColors[_currentIndex].bodyTextColor
-                            : Colors.black,
-                        fontSize: 20.0))
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(32.0),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: bgColors.length > 0
+                            ? bgColors[_currentIndex].color
+                            : Colors.white),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Nepal, The 8th Wonder",
+                          style: TextStyle(
+                              color: bgColors.isNotEmpty
+                                  ? bgColors[_currentIndex].titleTextColor
+                                  : Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30.0),
+                        ),
+                        const SizedBox(height: 10.0),
+                        Text(
+                            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id obcaecati tenetur enim et dolore aut dolorum! Fugiat omnis amet atque quos sapiente similique, tempore, vitae eos perferendis cupiditate libero odit.",
+                            textAlign: TextAlign.justify,
+                            style: TextStyle(
+                                color: bgColors.isNotEmpty
+                                    ? bgColors[_currentIndex].bodyTextColor
+                                    : Colors.black,
+                                fontSize: 20.0))
+                      ],
+                    ),
+                  ),
+                )
               ],
             ),
-          ),
-        )
-      ],
-    );
+          )
+        : Text("Yükleniyor");
   }
 }
